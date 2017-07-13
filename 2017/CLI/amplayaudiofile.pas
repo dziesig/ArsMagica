1c8cc33576adedc3c46643a7c24e889bd9ac55a4
Donald R. Ziesig <donald@ziesig.org>
2017-07-13 12:05:46 -0400

    audio player is now an object

--- 2017/CLI/amplayaudiofile.pas
+++ 2017/CLI/amplayaudiofile.pas
@@ -5,50 +5,91 @@ unit AMPlayAudioFile;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, Process;
 
-procedure PlayAudioFile( FilePath, FileName : String );
-procedure StopAudioFile;
+type
+
+  { TAudioFilePlayer }
+
+  TAudioFilePlayer = class(TProcess)
+    private
+      fPlayerPath : String;
+    public
+      constructor Create( thePlayerPath : String );
+      destructor  Destroy; override;
+
+      procedure Play( FilePath, FileName : String );
+      procedure Stop;
+
+      property PlayerPath : String read fPlayerPath;
+  end;
 
 implementation
 
 uses
-  Dos, Process,
+  Dos,
   AMStrings, AMDebug;
 
-var
-  Player : TProcess;
+//var
+//  Player : TProcess;
+//
+//procedure xPlayAudioFile(FilePath, FileName : String );
+//var
+//  PlayerPath : String;
+//  FullPath : String;
+//begin
+//  PlayerPath := GetEnv( 'AudioPlayer' );
+//  if Empty( PlayerPath ) then
+//    begin
+//      Debug('Environment variable [%s] undefined.',[PlayerPath]);
+//      exit;
+//    end;
+//  FullPath := '"' + FilePath + DirectorySeparator + FileName + '"';
+//  //Player := TProcess.Create( nil );
+//  //try
+//    Player.CommandLine := PlayerPath + ' ' + FullPath;
+//    Player.Execute;
+//  //finally
+//  //  Player.Free;
+//  //end;
+//end;
+//
+//procedure xStopAudioFile;
+//begin
+//  Player.Terminate(1);
+//end;
 
-procedure PlayAudioFile(FilePath, FileName : String );
-var
-  PlayerPath : String;
-  FullPath : String;
+{ TAudioFilePlayer }
+
+constructor TAudioFilePlayer.Create(thePlayerPath : String);
 begin
-  PlayerPath := GetEnv( 'AudioPlayer' );
-  if Empty( PlayerPath ) then
-    begin
-      Debug('Environment variable [%s] undefined.',[PlayerPath]);
-      exit;
-    end;
-  FullPath := '"' + FilePath + DirectorySeparator + FileName + '"';
-  //Player := TProcess.Create( nil );
-  //try
-    Player.CommandLine := PlayerPath + ' ' + FullPath;
-    Player.Execute;
-  //finally
-  //  Player.Free;
-  //end;
+  inherited Create( nil );
+  fPlayerPath := thePlayerPath;
 end;
 
-procedure StopAudioFile;
+destructor TAudioFilePlayer.Destroy;
 begin
-  Player.Terminate(1);
+  inherited Destroy;
 end;
 
-initialization
-  Player := TProcess.Create( nil );
-finalization
-  Player.Free;
+procedure TAudioFilePlayer.Play(FilePath, FileName : String);
+var
+  vFullPath : String;
+begin
+  vFullPath := '"' + FilePath + DirectorySeparator + FileName + '"';
+  CommandLine := PlayerPath + ' ' + vFullPath;
+  Execute;
+end;
+
+procedure TAudioFilePlayer.Stop;
+begin
+  Terminate(1);
+end;
+
+//initialization
+//  Player := TProcess.Create( nil );
+//finalization
+//  Player.Free;
 
 end.
 
